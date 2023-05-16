d1fc090b5140a57a3d00f8266a325b0fdd47ecaf
João Elias Maccari <joaomaccari@hotmail.com>
2023-05-16 14:36:28 -0300

    Revert "."
    This reverts commit d0cd05340e6c5a7f50b3aca6881bc2595ca895bc.

--- My project/Assets/Scripts/LootContainerInteractable.cs
+++ My project/Assets/Scripts/LootContainerInteractable.cs
@@ -7,7 +7,6 @@ public class LootContainerInteractable : Interactable
     [SerializeField] GameObject closedChest;
     [SerializeField] GameObject openedChest;
     [SerializeField] bool opened;
-
     public override void Interact(Character character) {
 
         if (opened == false) {
